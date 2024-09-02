7ddf15772ce4ffacbbc4211f1929bba58aa0b9d9
Minpyo Kim <pyo970120@gmail.com>
2024-09-02 16:22:33 +0900

    Build file update.
    Test code for compiled program has been added.
    
    Code for copying setting folder to build directory has been added.
    
    Some comment update.

--- build.py
+++ build.py
@@ -3,25 +3,43 @@ import subprocess
 import sys
 from pathlib import Path
 import shutil
+import subprocess
+
+# Global variable, flag to check if the build script is running under windows
+IS_WIN: bool = True if os.name == "nt" else False
 
 
 def is_venv() -> bool:
+    """Check if the script is running inside a virtual environment
+
+    Returns:
+        bool: True if it running inside of the virtual environment
     """
-    Check if the script is running inside a virtual environment.
-    """
+
     return hasattr(sys, "real_prefix") or (
         hasattr(sys, "base_prefix") and sys.base_prefix != sys.prefix
     )
 
 
 def activate_venv(venv_path) -> None:
-    """
-    Activate the virtual environment.
+    """Activate the virtual environment
 
-    :param venv_path: Path to the virtual environment.
+    Args:
+        venv_path (str): The path of virtual environment.
+
+    Raises:
+        FileNotFoundError: Activation Script is not found.
     """
-    activate_script = os.path.join(venv_path, "Scripts", "Activate.ps1")
-    if not os.path.exists(activate_script):
+
+    # Root of virtual environment
+    activate_script = Path(venv_path)
+
+    if IS_WIN:
+        activate_script.joinpath("Scripts", "Activate.ps1")
+    else:
+        activate_script.joinpath("bin", "activate")
+
+    if not activate_script.exists():
         raise FileNotFoundError(
             f"Activation script not found at {activate_script}"
         )
@@ -30,13 +48,15 @@ def activate_venv(venv_path) -> None:
         exec(f.read(), dict(__file__=activate_script))
 
 
-def build_with_nuitka(script_name, output_dir, ouput_name) -> None:
-    """
-    Build the specified Python script using Nuitka.
+def build_with_nuitka(script_name, output_dir, output_name) -> None:
+    """Build the specified Python script using Nuitka
 
-    :param script_name: The name of the Python script to compile.
-    :param output_dir: The directory to place the compiled executable.
+    Args:
+        script_name (str): The name of the Python script to compile.
+        output_dir (str): The directory to place the compiled executable.
+        output_name (str): The name of compiled executable.
     """
+
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -49,37 +69,75 @@ def build_with_nuitka(script_name, output_dir, ouput_name) -> None:
         "--onefile",
         "--remove-output",
         f"--output-dir={output_dir}",
-        f"--output-filename={ouput_name}",
+        f"--output-filename={output_name}",
         script_name,
     ]
 
     try:
         subprocess.run(command, check=True)
-        print(f"Successfully built {script_name} with Nuitka.")
+        print(f"\nSuccessfully built {output_name} with Nuitka.\n")
     except subprocess.CalledProcessError as e:
-        print(f"Failed to build {script_name}. Error: {e}")
+        print(f"\nFailed to build {output_name}. Error: {e}\n")
+        exit(2)
+
+
+def copy_setting_dir(output_dir) -> None:
+    """Copying directory that holding json files for ass header
+
+    Args:
+        output_dir (str): The output directory where excitable file
+    """
+
+    if Path(f"{output_dir}/setting").exists():
+        shutil.rmtree(Path(f"{output_dir}/setting"))
+
+    shutil.copytree(Path("./setting"), Path(f"{output_dir}/setting"))
+
+
+def test_build_prog(out_dir, output_name) -> None:
+    test_smis_dir = "./test_smis"
+
+    test_command = [
+        f"{smi2ass_prg}" + (".exe" if IS_WIN else ""),
+        f"{test_smis_dir}/Angel Beats! 01.smi",
+        f"{test_smis_dir}/Angel Beats! 02.smi",
+        f"{test_smis_dir}/Bakemonogatari-01.smi",
+        f"{test_smis_dir}/Durarara!! - 01.smi",
+        f"{test_smis_dir}/Durarara!! - 02.smi",
+        f"{test_smis_dir}/Psycho-Pass - S01E15.smi",
+        f"{test_smis_dir}/경계의 저편 BD 01화.smi",
+        f"{test_smis_dir}/경계의 저편 BD 02화.smi",
+    ]
+
+    print(test_command)
+    try:
+        subprocess.run(test_command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to run builded program. Error {e}")
+        exit(2)
 
 
 def main() -> None:
     # Ensure we are running in a virtual environment
     if not is_venv():
-        venv_path = "venv"  # Path to your virtual environment
+        venv_path = ".venv"  # Path to your virtual environment
         print(
-            f"Not running inside a virtual environment. Activating {venv_path}..."
+            f"\nNot running inside a virtual environment. Activating {venv_path}...\n"
         )
         activate_venv(venv_path)
 
     script_name: str = "./src/__main__.py"
     output_dir: str = "./build"
-    ouput_name: str = "smi2ass"
-    build_with_nuitka(script_name, output_dir, ouput_name)
+    output_name: str = "smi2ass"
 
-    # Check if setting file exist, if it is delete them
-    if Path(f"{output_dir}/setting").exists():
-        Path(f"{output_dir}/setting").unlink()
+    # Let's build the program
+    build_with_nuitka(script_name, output_dir, output_name)
 
-    # Copy setting folder into build folder
-    shutil.copytree(Path("./setting"), Path(f"{output_dir}/setting"))
+    # Let's copy json file for the ass header
+    copy_setting_dir(output_dir)
+
+    # Let's test program that build
+    test_build_prog(output_dir, output_name)
 
 
 if __name__ == "__main__":
